68b4b4c856cb048d78503132edd5e90725abe0b6
ImpostaWithBongos <127644479+ImpostaWithBongos@users.noreply.github.com>
2023-03-11 23:40:12 -0500

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -10,10 +10,10 @@ rm EncryptTemp.txt /dev/null 2>&1
 rm temp1.txt /dev/null 2>&1
 rm DecryptTemp.txt /dev/null 2>&1
 rm DecryptTemp2.txt /dev/null 2>&1
-apt remove apache2 -y
-apt remove mysql-server -y
-apt remove nmap -y
-apt remove wireshark -y 
+apt remove apache2 -y --purge 
+apt remove mysql-server -y --purge 
+apt remove nmap -y --purge 
+apt remove wireshark -y --purge
 rm /var/www/html/dump.csv /dev/null 2>&1
 rm CheatSheet.txt
 
