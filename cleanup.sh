09c9be346bf90c755db55f6c6baa54f9cb03f55c
ImpostaWithBongos <127644479+ImpostaWithBongos@users.noreply.github.com>
2023-03-12 12:46:44 -0700

    Add files via upload

--- cleanup.sh
+++ cleanup.sh
@@ -10,10 +10,10 @@ rm EncryptTemp.txt > /dev/null 2>&1
 rm temp1.txt > /dev/null 2>&1
 rm DecryptTemp.txt > /dev/null 2>&1
 rm DecryptTemp2.txt > /dev/null 2>&1
-apt remove apache2 -y --purge > /dev/null 2>&1
-apt remove mysql-server -y --purge > /dev/null 2>&1
-apt remove nmap -y --purge > /dev/null 2>&1
-apt remove wireshark -y --purge > /dev/null 2>&1
+apt autoremove apache2 -y > /dev/null 2>&1
+apt autoremove mysql-server -y > /dev/null 2>&1
+apt autoremove nmap -y > /dev/null 2>&1
+apt autoremove wireshark -y > /dev/null 2>&1
 rm /var/www/html/dump.csv > /dev/null 2>&1
 rm CheatSheet.txt > /dev/null 2>&1
 ip link set dev PCDC0 down
